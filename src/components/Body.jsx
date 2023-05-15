6b0275cda1e171d89a3ca998fd4d17af3aaedb1f
Aidan <aidangomar@gmail.com>
2023-05-15 16:22:54 -0700

    favicon and fonts

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,7 @@ const Body = () => {
     <div>
       <p>
         hi, my name is aidan<br></br><br></br>
-        this summer i'll be working at amazon as a software development engineer intern<br></br><br></br>
+        this summer i'm working at amazon as a software development engineer intern<br></br><br></br>
         i'm interested in distributed systems and database development<br></br><br></br>
         feel free to reach out
       </p>
